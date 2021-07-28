084abb3a265ac5297f78d57fc62d50938f6e7faa
JPBeckner <pedrobeckner@hotmail.com>
2021-07-27 23:51:36 -0300

    feat: upd server

--- server.py
+++ server.py
@@ -8,5 +8,5 @@ def teste():
     return "Teste"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True, load_dotenv=True)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8000, debug=True, load_dotenv=True)
